f3b9598f5495a68dac5fb9206ebe86a44235da82
sanavila <rute.leone@hotmail.com>
2023-03-08 09:42:44 -0300

    Navigation with <NuxtLink>

--- pages/fun.vue
+++ pages/fun.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>A vida não é só boleto!</h1>
+  <section>
+    <h1>A vida não é só boleto!</h1>
+    <NuxtLink to="/">Página inicial</NuxtLink>
+  </section>
 </template>
 <script lang="ts">
   export default {
