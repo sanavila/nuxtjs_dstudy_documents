cc983c43fa0c51dfc7bd25f06a0fb37f2d62bf78
sanavila <rute.leone@hotmail.com>
2023-03-08 09:38:20 -0300

    Automatic Routes in pages file

---
+++ pages/fun.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>A vida não é só boleto!</h1>
+</template>
+<script lang="ts">
+  export default {
+    name: "Fun"
+  }
+</script>
---
+++ pages/testederota.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Esse é um teste</h1>
+</template>
+<script lang="ts">
+export default {
+  name: "TesteDeRota"
+}
+</script>
